a17c866fc8920a80f4e6973ad54a4121e87caa10
Bharath547 <81628436+Bharath547@users.noreply.github.com>
2021-05-06 17:30:35 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,9 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[general]\n\
-email = \"bharathreddy1999d@gmail.com"\n\
-" > ~/.streamlit/credentials.tom1
-
-echo "\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
-" > ~/.streamlit/config.tom1
+" > ~/.streamlit/config.toml
